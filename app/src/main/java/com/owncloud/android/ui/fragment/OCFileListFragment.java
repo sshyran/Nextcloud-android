55e63b65cb1093ab132d611976a0ab77c08809c3
tobiasKaminsky <tobias@kaminsky.me>
2022-06-08 11:12:29 +0200

    When during renaming the same name already exists in same folder: show an error, disable confirm button
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1073,7 +1073,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
                             mContainerActivity.getFileOperationsHelper().openFileAsRichDocument(file, getContext());
                         } else {
                             // automatic download, preview on finish
-                            ((FileDisplayActivity) mContainerActivity).startDownloadForPreview(file);
+                            ((FileDisplayActivity) mContainerActivity).startDownloadForPreview(file, mFile);
                         }
                     }
                 }
@@ -1142,7 +1142,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
 
                 return true;
             } else if (itemId == R.id.action_rename_file) {
-                RenameFileDialogFragment dialog = RenameFileDialogFragment.newInstance(singleFile);
+                RenameFileDialogFragment dialog = RenameFileDialogFragment.newInstance(singleFile, mFile);
                 dialog.show(getFragmentManager(), FileDetailFragment.FTAG_RENAME_FILE);
                 return true;
             } else if (itemId == R.id.action_see_details) {
